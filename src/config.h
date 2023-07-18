20474eb1e2cce50d23f7fae30ea8bb980f93ddf5
mashalbasit <139747682+mashalbasit@users.noreply.github.com>
2023-07-18 12:20:43 +0500

    Update config.h

--- src/config.h
+++ src/config.h
@@ -15,7 +15,7 @@ struct Config {
 	file_format format;
 //    calculation_method method = ALEXANDER;
 	calculation_method method = LINKFIND;
-        double threshold = 256; 
+        std::pair<double, double> threshold = std::make_pair(20.0, 100.0);
 	int maxdim=2;;  // compute PH for these dimensions
 	bool print = false; // flag for printing parsistence pairs to stdout
 	bool verbose = false;
